693b20d6b31a1f5f1aadad853b59e5eb36a13315
晨魔 <139463472+MorningDemon515@users.noreply.github.com>
2025-01-26 20:04:20 +0800

    Sun of Light

--- Main.cpp
+++ Main.cpp
@@ -81,6 +81,19 @@ int main()
     -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     GLuint VAO,VBO;
     glGenBuffers(1,&VBO);
     glGenVertexArrays(1,&VAO);
@@ -224,7 +237,8 @@ int main()
 
         // be sure to activate shader when setting uniforms/drawing objects
         shader->Use();
-        glUniform3fv(shader->GetShaderSourceUniform("light.position"),1,&lightPos[0]);
+       // glUniform3fv(shader->GetShaderSourceUniform("light.position"),1,&lightPos[0]);
+        glUniform3f(shader->GetShaderSourceUniform("light.direction"), -0.2f, -1.0f, -0.3f);
         glUniform3fv(shader->GetShaderSourceUniform("viewPos"),1,&camera.cp[0]);
 
         // light properties
@@ -251,7 +265,18 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);;
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            glUniformMatrix4fv(shader->GetShaderSourceUniform("model"),1,GL_FALSE,&model[0][0]);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         // also draw the lamp object
         lightshader->Use();
@@ -263,7 +288,7 @@ int main()
         glUniformMatrix4fv(lightshader->GetShaderSourceUniform("model"),1,GL_FALSE,&model[0][0]);
 
         glBindVertexArray(lightCubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glDrawArrays(GL_TRIANGLES,0,36);
 
         SDL_GL_SwapWindow(window);
     }
